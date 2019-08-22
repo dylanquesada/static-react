8b387f2e8cb72f0676a1b642e8c4975940afd3ed
Dylan Quesada <dquesada@dylans-mbp.home>
2019-08-22 16:33:37 -0500

    Updates

---
+++ precache-manifest.f7e3d36deb42aa7fd6422a37d9065df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5443c835daec47fbad87125429d9261c",
+    "url": "/static-react/index.html"
+  },
+  {
+    "revision": "0de7cdf0556eb7f1851a",
+    "url": "/static-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f2e2a5ddb42fe760b445",
+    "url": "/static-react/static/js/2.48ed33a4.chunk.js"
+  },
+  {
+    "revision": "0de7cdf0556eb7f1851a",
+    "url": "/static-react/static/js/main.b05ca2e0.chunk.js"
+  },
+  {
+    "revision": "4695f6cb903033aeb10b",
+    "url": "/static-react/static/js/runtime~main.cc106b18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
